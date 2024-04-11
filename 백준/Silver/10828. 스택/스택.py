5d844bdfa9453fcbba792d3a913ed4d209bdc4de
OhJuYeong <142483228+JuYeong17@users.noreply.github.com>
2024-04-11 13:28:45 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,43 @@
+import sys
+N=int(sys.stdin.readline())
+
+stack=[]
+
+def push(a):
+    stack.append(a)
+
+def pop():
+    if stack:
+        print(stack.pop())
+    else:
+        print(-1)
+
+def size():
+    print(len(stack))
+
+def empty():
+    if not stack:
+        print(1)
+    else:
+        print(0)
+
+def top():
+    if stack:
+        print(stack[-1])
+    else:
+        print(-1)
+
+for i in range(N):
+    arr=sys.stdin.readline().split()
+
+    if arr[0]=='push':
+        push(arr[1])
+    elif arr[0] =='pop':
+        pop()
+    elif arr[0]=='size':
+        size()
+    elif arr[0]=='empty':
+        empty()
+    elif arr[0]=='top':
+        top()
+
