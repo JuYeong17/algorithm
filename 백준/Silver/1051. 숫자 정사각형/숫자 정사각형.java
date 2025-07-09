a320677ae7acb9beeb3752e0df90ea3bdfae4142
OhJuYeong <142483228+JuYeong17@users.noreply.github.com>
2025-07-09 23:30:10 +0900

    [Silver III] Title: 숫자 정사각형, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int N,M, ans=Integer.MIN_VALUE;
+  static int[][] map;
+  public static void main(String[] args) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(bf.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    map = new int[N][M];
+    for(int i=0;i<N;i++){
+      String str= bf.readLine();
+      for(int j =0; j<M;j++){
+        map[i][j] = str.charAt(j) - '0';
+      }
+    }
+    int len = Math.min(N,M);
+
+    while(len > 1){
+      for ( int i=0; i<=N-len;i++){
+        for ( int j= 0; j<=M-len;j++){
+          int num = map[i][j];
+          if (num == map[i][j + len - 1] && num == map[i + len - 1][j] && num == map[i+len-1][j+len-1]){
+            System.out.println(len * len);
+            return;
+          }
+        }
+      }
+      len--;
+    }
+    System.out.println(len * len);
+  }
+}
