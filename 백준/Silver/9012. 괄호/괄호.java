cdca8b2fcfd7a7c1a35285f7ab063ddbfe030872
OhJuYeong <142483228+JuYeong17@users.noreply.github.com>
2025-09-25 23:09:19 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		Stack<Character> stack=new Stack<>();
+		int T=Integer.parseInt(br.readLine());
+		for(int i=0;i<T;i++) {
+			String num=br.readLine();
+			for(int j=0;j<num.length();j++) {
+				char bra=num.charAt(j);
+				if(bra == '(') {
+					stack.push(bra);
+				}
+				else if(stack.empty()) {
+					stack.push(')');
+					break;
+				}
+				else {
+					stack.pop();
+				}
+			}
+			
+			if(stack.empty()) {
+				System.out.println("YES");
+			}
+			else {
+				System.out.println("NO");
+			}
+			stack.clear();
+			}
+		}
+	}
