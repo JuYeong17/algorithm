f1da20dc130531276ac46dac8015a5812e15cb76
OhJuYeong <142483228+JuYeong17@users.noreply.github.com>
2025-12-02 16:43:41 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 100 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -2,28 +2,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int[] M = new int[9];
-    int result = 0;
-    for (int i= 0; i<9;i++){
-      M[i] =  Integer.parseInt(br.readLine());
-      result += M[i];
+    int[] input = new int[9];
+    int total = 0;
+    for(int i=0;i<9;i++){
+      input[i] = Integer.parseInt(br.readLine());
+      total += input[i];
     }
-    for(int i=0;i<8;i++){
-      for(int j= i+1; j<9;j++){
-        if(result -M[i] -M[j] ==100){
-          M[i] = 0;
-          M[j] = 0;
-          Arrays.sort(M);
-          for( int k=2;k<9;k++){
-            System.out.println(M[k]);
-          }
-          return;
+    boolean found = false;
+
+    for(int i=0;i< 9;i++){
+      for(int j=1;j< 9;j++){
+        int result = input[i] + input[j];
+        if(result == total - 100){
+          input[i] = 0;
+          input[j] = 0;
+          found = true;
+          break;
         }
       }
+      if(found) break;
+    }
+    Arrays.sort(input);
+    for(int i=2;i<9;i++){
+      System.out.println(input[i]);
     }
   }
 }
