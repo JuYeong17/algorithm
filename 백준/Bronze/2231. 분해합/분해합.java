8aff6429a588c25206286c1166f0b87f54efa057
OhJuYeong <142483228+JuYeong17@users.noreply.github.com>
2025-12-02 11:51:47 +0900

    [Bronze II] Title: 분해합, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,27 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();  
-        int answer = 0; 
-
-        for (int i = 1; i < N; i++) {
-            int sum = i;
-            int temp = i;
-
-            // 각 자리수 더하기
-            while (temp > 0) {
-                sum += temp % 10; 
-                temp /= 10;        
-            }
-
-            if (sum == N) {
-                answer = i;
-                break; 
-            }
-        }
-
-        System.out.println(answer);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int len =  String.valueOf(n).length();
+    int start =Math.max(1, n - 9*len);
+    for(int i = start ; i<n;i++){
+      int sum = i;
+      int temp = i;
+      while(temp>0){
+        sum += temp %10;
+        temp /=10;
+      }
+      if(sum ==n){
+        System.out.println(i);
+        return;
+      }
     }
+    System.out.println(0);
+  }
 }
