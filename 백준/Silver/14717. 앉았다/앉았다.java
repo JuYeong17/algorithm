e799a7e1794c77c81b13e8feb957b8f5a9e37edb
OhJuYeong <142483228+JuYeong17@users.noreply.github.com>
2025-12-04 16:02:07 +0900

    [Silver IV] Title: 앉았다, Time: 112 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Silver/14717. 앉았다/앉았다.java
+++ 백준/Silver/14717. 앉았다/앉았다.java
@@ -1,34 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
+  public static int score(int a, int b){
+    if ( a == b) return a *10;
+    return (a+b) % 10;
+  }
+
   public static void main(String[] args) throws IOException {
-    DecimalFormat df = new DecimalFormat("0.000");
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] input = br.readLine().split(" ");
-    int N = Integer.parseInt(input[0]);
-    int M = Integer.parseInt(input[1]);
-    int cases = 153;
-    int count =0;
-    if(N==M){
-      count = cases - (10 - N);
-      System.out.println(df.format(count / (cases * 1.0)));
-    } else{
-      for( int i=1; i<=10;i++){
-        for(int j=1+i;j<=10;j++) {
-          int sum = i + j;
-          if (sum % 10 < (N + M) % 10) {
-            if (i == N | i == M | j == N | j == M) {
-              count += 2;
-            } else {
-              count += 4;
-            }
-          }
-        }
-      }
-      System.out.println(df.format(count / (cases *1.0)));
+    StringTokenizer st = new StringTokenizer((br.readLine()));
+    int x= Integer.parseInt(st.nextToken());
+    int y= Integer.parseInt(st.nextToken());
+
+    List<Integer> deck = new ArrayList<>();
+    for( int i=1;i<=10;i++){
+      deck.add(i);
+      deck.add(i);
     }
+    deck.remove((Integer) x);
+    deck.remove((Integer) y);
+    int Myscore = score(x,y);
+    int win=0;int total=0;
+
+    for(int i=0;i< deck.size();i++){
+      for(int j=i+1;j< deck.size();j++){
+        int x2 = deck.get(i);
+        int y2 = deck.get(j);
+        int YourScore =score(x2,y2);
+        if(Myscore > YourScore) win++;
+        total++;
+      }
+    }
+    System.out.printf("%.3f\n",(double) win/ total);
   }
 }
