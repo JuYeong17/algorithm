181461b10b7835ae31c5724ddf72ea3dc11afbd6
OhJuYeong <142483228+JuYeong17@users.noreply.github.com>
2025-12-28 16:42:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.78 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,36 +1,34 @@
 import java.util.*;
 class Solution {
-    static int[][] visited;
-    static Queue<int[]> q;
     public int solution(int[][] maps) {
-        visited = new int[maps.length][maps[0].length];
-        q= new LinkedList<>();
+        int n= maps.length;
+        int m = maps[0].length;
+        int[][] visited = new int[n][m];
+        Queue<int[]> q= new ArrayDeque<>();
         
         visited[0][0]= 1;
         q.add(new int[]{0,0});
-        bfs(maps);
-        int result = visited[maps.length-1][maps[0].length-1];
-        if (result == 0) return -1;
-        else return result;
-    }
-    
-    public void bfs(int[][] maps){
-        int[] dx = {-1,1,0,0};
+        
+        int[] dx= {-1,1,0,0};
         int[] dy = {0,0,1,-1};
+        
         while(!q.isEmpty()){
             int[] cur= q.poll();
             int x = cur[0];
             int y = cur[1];
             for(int i=0;i<4;i++){
-                int nx= x + dx[i];
-                int ny = y +dy[i];
-                if(nx <0||nx>=maps.length||ny<0||ny>=maps[0].length) continue;
-                else if(maps[nx][ny]==1 && visited[nx][ny] == 0){
-                    visited[nx][ny] = visited[x][y] + 1;
-                    q.add(new int[]{nx,ny});
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx < 0 || nx>=n||ny<0||ny>=m) continue;
+                if(visited[nx][ny] !=0 || maps[nx][ny] == 0) continue;
+                visited[nx][ny] = visited[x][y]+ 1;
+
+                if(nx == n-1 && ny == m-1){
+                    return visited[nx][ny];
                 }
+                q.add(new int[]{nx,ny});
             }
         }
+        return -1;
     }
-
 }
